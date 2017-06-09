fd7b6b9d2f695761b3efb94ded73abb574ea4a5b
Ivandro Ismael <ivandrofly@gmail.com>
2017-06-09 04:42:15 +0000

    make all the api method go through one method to make request

--- src/Http/SubDBApi.cs
+++ src/Http/SubDBApi.cs
@@ -47,12 +47,12 @@ namespace SubDBSharp
             _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", Utils.FormatUserAgent(productInformation));
         }
 
-        public async Task<Response> GetAvailableLanguagesAsync()
+        public Task<Response> GetAvailableLanguagesAsync()
         {
             var uriBuilder = new UriBuilder(BaseAddress) { Query = "action=languages" };
-            using (HttpResponseMessage responseMessage = await _httpClient.GetAsync(uriBuilder.Uri).ConfigureAwait(false))
+            using (HttpRequestMessage requestMessage = BuildRequestMessage(uriBuilder.Uri, HttpMethod.Get, null))
             {
-                return await BuildResponse(responseMessage);
+                return SendDataAsync(requestMessage);
             }
         }
 
@@ -62,14 +62,13 @@ namespace SubDBSharp
         /// <param name="hash">The parameter hash is the hash of the video file, generated using our hash function.</param>
         /// <param name="getVersions">This parameter is optional. Using it to return how many versions per language of a subtitle we have in our database.</param>
         /// <returns></returns>
-        public async Task<Response> SearchSubtitle(string hash, bool getVersions = false)
+        public Task<Response> SearchSubtitle(string hash, bool getVersions = false)
         {
             var fullUrl = SubDBApiUrl.ApplySearchSubtitleParameters(hash, getVersions);
-            using (HttpResponseMessage resonseMessage = await _httpClient.GetAsync(fullUrl).ConfigureAwait(false))
+            using (HttpRequestMessage requestMessage = BuildRequestMessage(fullUrl, HttpMethod.Get, null))
             {
-                //return await resonseMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
-                return await BuildResponse(resonseMessage);
-            };
+                return SendDataAsync(requestMessage);
+            }
         }
 
         /// <summary>
@@ -79,27 +78,12 @@ namespace SubDBSharp
         /// <param name="languages"> The parameter language can be a single language code (ex.: us), or a comma separated list in order of priority (ex.: us,nl).
         /// //When using a comma separated list, the first subtitle found is returned.</param>
         /// <returns></returns>
-        public async Task<Response> DownloadSubtitle(string hash, params string[] languages)
+        public Task<Response> DownloadSubtitle(string hash, params string[] languages)
         {
             var fullUrl = SubDBApiUrl.ApplyDownloadSubtitleParameters(hash, languages);
-            using (HttpResponseMessage responseMessage = await _httpClient.GetAsync(fullUrl, HttpCompletionOption.ResponseContentRead).ConfigureAwait(false))
+            using (var requestMessage = BuildRequestMessage(fullUrl, HttpMethod.Get, null))
             {
-                //var responseBody = await responseMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
-                //string fileName = string.Empty;
-                //if (responseMessage.Content.Headers.ContentDisposition != null)
-                //{
-                //    fileName = responseMessage.Content.Headers.ContentDisposition.FileName;
-                //}
-#if DEBUG
-                // according to website "the language of the downloaded subtitle is returned in the
-                // content-language field in the header of the response" but it seems to not work :(...
-                //if (responseMessage.Content.Headers.ContentLanguage.Any())
-                //{
-                //    // todo: retrive languge.
-                //    // note: this is only usefull if several languages were passed in languages parameter
-                //}
-#endif
-                return await BuildResponse(responseMessage);
+                return SendDataAsync(requestMessage);
             }
         }
 
@@ -108,45 +92,33 @@ namespace SubDBSharp
         /// </summary>
         /// <param name="request"></param>
         /// <returns></returns>
-        public async Task<Response> UploadSubtitle(string subtitle, string movie)
+        public Task<Response> UploadSubtitle(string subtitle, string movie)
         {
-            const string dispositionType = "form-data";
-            using (var content = new MultipartFormDataContent("xYzZY"))
+            var uriBuilder = new UriBuilder(BaseAddress) { Query = "action=upload" };
+            using (HttpContent httpContent = CreateFormContent(subtitle, movie))
+            using (HttpRequestMessage httpRequestMessage = BuildRequestMessage(uriBuilder.Uri, HttpMethod.Post, httpContent))
             {
-                // hash info
-                StringContent stringContent = stringContent = new StringContent(Utils.GetMovieHash(movie));
-                stringContent.Headers.ContentDisposition = new ContentDispositionHeaderValue(dispositionType)
-                {
-                    Name = "\"hash\""
-                };
-
-                // subtitle file info
-                FileStream fs = File.OpenRead(subtitle);
-                var streamContent = new StreamContent(fs);
-                streamContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
-                var dispo = new ContentDispositionHeaderValue(dispositionType)
-                {
-                    Name = "\"file\"",
-                    FileName = $"\"{Path.GetFileName(subtitle)}\""
-                };
-                streamContent.Headers.ContentDisposition = dispo;
-
-                // add contents to form
-                content.Add(stringContent);
-                content.Add(streamContent);
-
-                var uriBuilder = new UriBuilder(BaseAddress) { Query = "action=upload" };
-                using (HttpResponseMessage response = await _httpClient.PostAsync(uriBuilder.Uri, content))
-                {
-                    return await BuildResponse(response);
-                }
+                return SendDataAsync(httpRequestMessage);
             }
         }
 
-        protected virtual HttpRequestMessage BuildRequestMessage(Request request)
+        // ALL THE METHOD MUST GO THROUGH THIS!!!
+        private async Task<Response> SendDataAsync(HttpRequestMessage requestMessage)
         {
-            // all the requests must go through this method
-            throw new NotImplementedException();
+            using (HttpResponseMessage responseMessage = await _httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseContentRead).ConfigureAwait(false))
+            {
+                return await BuildResponse(responseMessage);
+            }
+        }
+
+        protected virtual HttpRequestMessage BuildRequestMessage(Uri endPoint, HttpMethod httpMethod, HttpContent content)
+        {
+            return new HttpRequestMessage()
+            {
+                RequestUri = endPoint,
+                Method = httpMethod,
+                Content = content
+            };
         }
 
         protected virtual async Task<Response> BuildResponse(HttpResponseMessage responseMessage)
@@ -165,5 +137,34 @@ namespace SubDBSharp
                 responseMessage.Headers.ToDictionary(h => h.Key, h => h.Value.First()));
         }
 
+        private static HttpContent CreateFormContent(string subtitle, string movie)
+        {
+            const string dispositionType = "form-data";
+            var content = new MultipartFormDataContent("xYzZY");
+
+            // hash info
+            StringContent stringContent = stringContent = new StringContent(Utils.GetMovieHash(movie));
+            stringContent.Headers.ContentDisposition = new ContentDispositionHeaderValue(dispositionType)
+            {
+                Name = "\"hash\""
+            };
+
+            // subtitle file info
+            FileStream fs = File.OpenRead(subtitle);
+            var streamContent = new StreamContent(fs);
+            streamContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            var dispo = new ContentDispositionHeaderValue(dispositionType)
+            {
+                Name = "\"file\"",
+                FileName = $"\"{Path.GetFileName(subtitle)}\""
+            };
+            streamContent.Headers.ContentDisposition = dispo;
+
+            // add contents to form
+            content.Add(stringContent);
+            content.Add(streamContent);
+
+            return content;
+        }
     }
 }
